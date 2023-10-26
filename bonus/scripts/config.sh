fda0463a9b0a395f0f640eac1acec3c71b2b7f95
mustapha rachidi <mustapharachidipro@gmail.com>
2023-10-26 22:42:16 +0100

    update bonus gitlab

--- bonus/scripts/config.sh
+++ bonus/scripts/config.sh
@@ -12,6 +12,7 @@ kubectl create namespace gitlab
 
 echo -e "${GREEN} apply argocd ...${ENDCOLOR}"
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/v2.3.5/manifests/install.yaml
+sleep 10
 ./waiting-argocd.sh
 
 kubectl apply -f ../confs/application.yaml
@@ -28,13 +29,12 @@ echo -e "${GREEN} ====================================================${ENDCOLOR
 echo -e "${GREEN} install Helm repo gitlab ...${ENDCOLOR}"
 helm upgrade --install gitlab gitlab/gitlab --timeout 600s \
 --set global.hosts.domain=gitlab.murachid.com   \
---set certmanager-issuer.email=abcd@gmail.com \
+--set certmanager-issuer.email=mustapharachidpro@gmail.com \
 --set global.hosts.https=false  \
---set gitlab.initialRootPassword=PASSWORD1337 \
---set gitlab-runner.install="false" \
---set gitlab.rootUser.username=murachid -n gitlab
+--set gitlab-runner.install="false" -n gitlab
 echo -e "${GREEN} ====================================================${ENDCOLOR}"
-./waiting-gitlab.sh
+sleep 10
+# ./waiting-gitlab.sh
 
 echo -e "${GREEN} Get gitlab-gitlab-initial-root-password ...${ENDCOLOR}"
 kubectl get secret -n gitlab gitlab-gitlab-initial-root-password -o jsonpath='{.data.password}' | base64 -d; echo
@@ -42,5 +42,5 @@ echo -e "${GREEN} ====================================================${ENDCOLOR
 
 
 echo -e "${GREEN} forwarding  ...${ENDCOLOR}"
-kubectl port-forward --address 0.0.0.0 svc/gitlab-webservice-default -n gitlab 8686:8181  | kubectl port-forward --address 0.0.0.0 svc/argocd-server -n argocd 8080:443
+kubectl port-forward --address 0.0.0.0 svc/gitlab-webservice-default -n gitlab 9009:8181  | kubectl port-forward --address 0.0.0.0 svc/argocd-server -n argocd 8080:443
 echo -e "${GREEN} ====================================================${ENDCOLOR}"
