6e9dc3716b23f87bf93168feb9a43a47bae2766b
mustapha Rachidi <mustapharachidipro@gmail.com>
2023-11-17 10:01:29 +0100

    ubpdate

--- p1/scripts/install-master.sh
+++ p1/scripts/install-master.sh
@@ -5,8 +5,7 @@ BLUE='\033[0;34m'
 ENDCOLOR='\033[0m'
 
 
-sudo sed -i 's/PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
-sudo systemctl restart ssh
+
 export INSTALL_K3S_EXEC="server --write-kubeconfig-mode 644  --advertise-address=192.168.59.140 --node-ip=192.168.59.140"
 curl -sfL https://get.k3s.io |  sh -
 cp /var/lib/rancher/k3s/server/node-token /vagrant/
--- p1/scripts/install-worker.sh
+++ p1/scripts/install-worker.sh
@@ -6,7 +6,5 @@ ENDCOLOR='\033[0m'
 
 
 
-sudo sed -i 's/PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
-sudo systemctl restart ssh
 export INSTALL_K3S_EXEC="agent --server https://192.168.59.140:6443 --token-file /vagrant/node-token --node-ip=192.168.59.141"
 curl -sfL https://get.k3s.io |  sh -
