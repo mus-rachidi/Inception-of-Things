a76c4ed576ca12199803c36c46d28bcbd46f5842
Mustapha Rachidi <mustapharachidipro@gmail.com>
2023-12-23 19:32:43 +0100

    update

--- p1/scripts/master.sh
+++ p1/scripts/master.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export INSTALL_K3S_EXEC="server --write-kubeconfig-mode 644  --advertise-address=192.168.56.112 --node-ip=192.168.56.112"
+export INSTALL_K3S_EXEC="server --write-kubeconfig-mode 644  --advertise-address=MASTER_IP --node-ip=MASTER_IP"
 curl -sfL https://get.k3s.io |  sh -
 cp /var/lib/rancher/k3s/server/node-token /vagrant/confs/
 echo "alias k='k3s kubectl'" >> /etc/profile.d/apps-bin-path.sh
